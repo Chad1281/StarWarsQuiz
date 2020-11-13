a76edeb822428b04b369e8185ee5b53ac85678c6
Chad Cook <Charles.Cook28@gmail.com>
2020-11-12 23:40:33 -0600

    added the scoreboard

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# star_wars_quiz
+# Star Wars Quiz
+
+## Description
+
+This is a quiz to test your knowledge of the Star Wars movies.
+
+![]()
+
+## Deployment
+
+Access the page at [https://chad1281.github.io/star_wars_quiz/](https://chad1281.github.io/star_wars_quiz/)
+
+## Utilization
+
+1. Read the instructions before clicking the start quiz button.
+2. Once you have finished reading the instructions then the timer will start and you will see a question with 4 choices below.
+3. Click the choice which you think is correct.
+4. After all of the questions are finished you will see your score and can save it by entering your name and hitting submit.
+
+![]()# star_wars_quiz
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,92 @@
-var unHide = document.createElement("div");
+// select timer element
+var timeEl = document.querySelector("#timer");
+var secondsLeft = 60;
+
+// create elements for questions container
+var unHideQc = document.createElement("div");
 var questCon = document.createElement("div");
 var ansCon = document.createElement("div");
+var nameFoCon = document.createElement("div");
 var questH2 = document.createElement("h2");
 var startBtn = document.getElementById("start");
 var hideOnStart = document.querySelector("#hideOnStart");
 var container = document.querySelector(".container");
-var timeEl = document.querySelector("#timer");
-var secondsLeft = 60;
 
+// create elements for form container
+var unHideFo = document.createElement("div");
+var formCon = document.createElement("div");
+var formH2 = document.createElement("h2");
+var form = document.createElement("form");
+var formLabel = document.createElement("label");
+var formInput = document.createElement("input");
+var formBtn = document.createElement("input");
+var formBr = document.createElement("br");
+
+// create variables to shuffle and select questions from question bank
 let shuffledQuestions, currentQuestIndex
 
-unHide.setAttribute("class", "hide");
+// create elements for scoreboard
+var unHideSB = document.createElement("div");
+var scoreCon = document.createElement("div");
+var scoreH2 = document.createElement("h2");
+var scoreBtn = document.createElement("button");
+var scoreOl = document.createElement("ol");
+var scoreboard = document.getElementById("scoreboard");
+
+// set classes for question elements
+unHideQc.setAttribute("class", "hide");
 questCon.setAttribute("class", "quizCon");
 ansCon.setAttribute("class", "ansConClass");
 
-container.appendChild(unHide);
-unHide.appendChild(questCon);
+// set attributes for form elements
+unHideFo.setAttribute("class", "hide");
+nameFoCon.setAttribute("class", "quizCon");
+formCon.setAttribute("class", "quizCon");
+form.setAttribute("id", "nameForm");
+formInput.setAttribute("type", "text");
+formInput.setAttribute("id", "nameInput");
+formBtn.setAttribute("type", "submit");
+formBtn.setAttribute("value", "Submit Name");
+formBtn.setAttribute("class", "btn");
+
+// set classes for scoreboard elements
+unHideSB.setAttribute("class", "hide");
+unHideSB.setAttribute("id", "scores");
+scoreCon.setAttribute("class", "quizCon");
+scoreBtn.setAttribute("class", "btn");
+
+// place question elements
+container.appendChild(unHideQc);
+unHideQc.appendChild(questCon);
 questCon.appendChild(questH2);
 questCon.appendChild(ansCon);
 
+// place form elements
+container.appendChild(unHideFo);
+unHideFo.appendChild(nameFoCon);
+nameFoCon.appendChild(formCon);
+formCon.appendChild(formH2);
+formCon.appendChild(form);
+form.appendChild(formLabel);
+form.appendChild(formInput);
+form.appendChild(formBr);
+form.appendChild(formBtn);
+
+// place question elements
+container.appendChild(unHideSB);
+unHideSB.appendChild(scoreCon);
+scoreCon.appendChild(scoreBtn);
+scoreCon.appendChild(scoreH2);
+scoreCon.appendChild(scoreOl);
+
+// text for form label
+formLabel.textContent = "Enter you name for scoreboard:";
+
+// text for scoreboard
+scoreH2.textContent = "High Scores";
+scoreBtn.textContent = "Play Again";
+
+// question index with choice index and correct answers
 const questions = [
     {
         question: "What year did the first Star Wars movie come out?",
@@ -50,48 +118,59 @@ const questions = [
         choices: ["Luke Skywalker", "Anakin Skywalker", "Obi-Wan Kenobi", "Han Solo"],
         answer: "Anakin Skywalker"
     },
+    {
+        question: "What is the name of the character played by Mark Hamill?",
+        choices: ["Anakin Skywalker", "Luke Skywalker", "Darth Vader", "Han Solo"],
+        answer: "Luke Skywalker"
+    },
+    {
+        question: "Which are the only 2 characters to be in every Star Wars movie?",
+        choices: ["Obi-Wan Kenobi, Darth Vader", "Luke Skywalker, Princess Leia", "R2D2, C3PO", "Han Solo, Chewbacca"],
+        answer: "R2D2, C3PO"
+    },
 ]
 
+// start quiz on click
 startBtn.addEventListener("click", startQuiz); 
 
+// hides instructions and unhides question elements then gets questions ready and starts timer
 function startQuiz() {
     console.log("started");
     hideOnStart.classList.add("hide");
-    unHide.classList.remove("hide");
+    unHideSB.classList.add("hide");
+    unHideQc.classList.remove("hide");
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestIndex = 0
     setTime();
     nextQuest();
 }
 
+// displays time left and ends game when time runs out, sets score
 function setTime() {
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timeEl.textContent = "Time left: " + secondsLeft;
   
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
+      if(secondsLeft <= 0) {
+        formH2.textContent = "Your score is " + secondsLeft; 
+        clearInterval(timerInterval); 
+        endGame(); 
         sendMessage();
       }
-  
     }, 1000);
 }
 
 function sendMessage() {
     console.log("times up");
-    // timeEl.textContent = " ";
-  
-    // var imgEl = document.createElement("img");
-  
-    // imgEl.setAttribute("src", "images/image_1.jpg");
-    // mainEl.appendChild(imgEl);  
   }
 
+// runs resetState & showQuest functions  
 function nextQuest() {
     resetState();
     showQuest(shuffledQuestions[currentQuestIndex]);
 }
 
+// inserts question and choices into question container, checks user answer and moves to next question
 function showQuest(question) {
     questH2.innerText = question.question;
     console.log(question.choices);
@@ -99,31 +178,18 @@ function showQuest(question) {
         const button = document.createElement("button");
         button.innerHTML = choice;
         button.classList.add("btn");
-        var ansObj = question.answer;
-        if (ansObj) {
-            button.dataset.correct = ansObj;
-        }
+        button.setAttribute("data-name", choice);
+        button.setAttribute("data-correct", question.answer);
         button.addEventListener("click", selectAns);
         button.addEventListener("click", () => {
             currentQuestIndex++;
             nextQuest();
         })
         ansCon.appendChild(button);
-        
-        
-        // function checkAns() {
-        //     if (button.innerHTML == question.answer) {
-        //         console.log("correct");
-        //         nextQuest();
-        //     }
-        //     else {
-        //         console.log("wrong");
-        //         nextQuest();
-        //     }
-        // }
     });
 }
 
+// removes last question and choices from question elements
 function resetState() {
     while (ansCon.firstChild) {
         ansCon.removeChild
@@ -131,34 +197,72 @@ function resetState() {
     }
 }
 
+// compares user to the correct answer and adds or removes time from the timer, ends game when after last question
 function selectAns(e) {
     const selectedBtn = e.target;
     console.log(selectedBtn);
-    const correct = selectedBtn.dataset.correct;
-    // setStatus(document.body, correct);
-    Array.from(questCon.children).forEach(button => {
-        setStatus(button, button.dataset.correct)
-    })
-}
-
-function setStatus(element, correct) {
-    // clearStatus(element);
-    if (correct) {
-        console.log("Correct");
+    var corAns = selectedBtn.dataset.correct;
+    console.log(corAns);
+    var userSel = selectedBtn.dataset.name;
+    console.log(userSel);
+    if (userSel == corAns) {
+        console.log("correct");
+        secondsLeft += 20;
     }
-    else {console.log("Wrong")}
+    else {
+        console.log("wrong");
+        secondsLeft -= 20;
+    }
+    if (shuffledQuestions.length <= currentQuestIndex + 1) {
+        console.log("endgame");
+        endGame();
+    }
 }
 
-// for (i = 0; i < questions.length; i++) {
-//     // console.log(questions[i]);
-//     var questEl = (questions[i].question);
-//     var ansBtn0 = (questions[i].choices[0]);
-//     var ansBtn1 = (questions[i].choices[1]);
-//     var ansBtn2 = (questions[i].choices[2]);
-//     var ansBtn3 = (questions[i].choices[3]);
-//     questH2.innerHTML = questEl;
-//     conBtn0.innerHTML = ansBtn0;
-//     conBtn1.innerHTML = ansBtn1;
-//     conBtn2.innerHTML = ansBtn2;
-//     conBtn3.innerHTML = ansBtn3;
-// }
+// hides question elements and unhides form elements
+function endGame() {
+    unHideQc.classList.add("hide");
+    unHideFo.classList.remove("hide");
+}
+
+// brings up scoreboard on click
+formBtn.addEventListener("click", submitScore);
+scoreboard.addEventListener("click", showScores);
+
+// saves score to scoreboard
+function submitScore() {
+    var names = document.querySelector("#nameInput").value;
+    var scores = secondsLeft;
+    unHideFo.classList.add("hide");
+    localStorage.setItem("names", names);
+    localStorage.setItem("scores", scores);
+    showScores();
+}
+
+// shows the scoreboard
+function showScores() {
+    console.log("show scores");
+    hideOnStart.classList.add("hide");
+    unHideSB.classList.remove("hide");
+    var highScore = localStorage.getItem("names") + localStorage.getItem("scores");
+    const li = document.createElement("li");
+    li.innerHTML = highScore;
+    scoreOl.appendChild(li);
+    // scoreOl.appendChild("li");
+}
+
+// starts game over
+scoreBtn.addEventListener("click", playAgain);
+
+// returns to the start quiz screen
+function playAgain() {
+    unHideSB.classList.add("hide");
+    hideOnStart.classList.remove("hide");
+    resetTimer();
+}
+
+// resets timer for new game
+function resetTimer() {
+    var secondsLeft = 60;
+    timeEl.textContent = "Time Left: " + secondsLeft;
+}
